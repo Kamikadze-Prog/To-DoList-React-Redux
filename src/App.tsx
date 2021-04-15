a798fb3f7f6e45ddf8a296e54762709bd25cc79d
Kamikadze-Prog <kamikadze19900@gmail.com>
2021-04-15 16:13:07 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App(): JSX.Element {
+function App(): JSX.Elem
+ent {
   return (
     <div className="App">
       <header className="App-header">
